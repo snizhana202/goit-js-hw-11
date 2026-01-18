c22d2d40a18751ddca7c065b383f51bc22288ce5
Snizhana <snizhanapetrushka@gmail.com>
2026-01-18 10:15:06 +0100

    changes

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,6 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
